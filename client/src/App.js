a64d0df55ac5929ed57da3a6ec75373511dd9246
Kevin Lee <kevin.rph.lee@gmail.com>
2018-11-25 14:40:35 -0800

    created home and login routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import fire from './config/Fire.js';
 import Home from './components/Home.js';
 import Login from './components/Login.js';
+import {HashRouter,
+  Switch,
+  Route,
+  Link, BrowserRouter, Redirect, withRouter} from 'react-router-dom';
+
+
 
 class App extends Component {
   constructor() {
@@ -19,11 +25,12 @@ class App extends Component {
 
   authListener() {
     fire.auth().onAuthStateChanged((user) => {
-      console.log(user);
       if (user) {
+        console.log('Yes')
         this.setState({ user });
         localStorage.setItem('user', user.uid);
       } else {
+        console.log('No')
         this.setState({ user: null });
         localStorage.removeItem('user');
       }
@@ -32,12 +39,14 @@ class App extends Component {
 
   render() {
     return (
-     <div>
-       <p>Hello</p>
-
-     </div>
+      <div>
+        <Switch>
+          <Route path='/login' render={(props) => <Login />} />
+          <Route path='/' render={(props) => <Home />} />
+        </Switch>
+      </div>
     )
   }
 }
 
- export default App;
+ export default withRouter(App);
