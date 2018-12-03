e4f32d2d4f4e7efe22ba4013877056ea8bacdd67
Kevin Lee <kevin.rph.lee@gmail.com>
2018-12-02 19:17:53 -0800

    created login modal template

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import fire from './../config/Fire';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Modal from 'react-modal';
 
 
 import {
@@ -17,20 +18,41 @@ import {
   DropdownMenu,
   DropdownItem } from 'reactstrap';
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
 
 class AppNavBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      dropDownIsOpen: false
+      dropDownIsOpen: false,
+      modalIsOpen: false
+
     };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+
   }
 
 
-  // componentDidMount() {
-  //   console.log(this.state.user)
-  // }
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
 
   toggleNavBarDropDown = () => {
     this.setState({
@@ -58,6 +80,17 @@ class AppNavBar extends Component {
                       Option 2
                     </DropdownItem>
                     <DropdownItem divider />
+                    <DropdownItem onClick={this.openModal}>Open Modal</DropdownItem>
+                    <Modal
+                      isOpen={this.state.modalIsOpen}
+                      onAfterOpen={this.afterOpenModal}
+                      onRequestClose={this.closeModal}
+                      style={customStyles}
+                      contentLabel="Example Modal"
+                    >
+                      <h1>Login</h1>
+                      <button onClick={this.closeModal}>close</button>
+                    </Modal>
                   </DropdownMenu>
                 </UncontrolledDropdown>
     } else {
@@ -94,6 +127,7 @@ class AppNavBar extends Component {
             </Nav>
           </Collapse>
         </Navbar>
+
       </div>
     )
   }
